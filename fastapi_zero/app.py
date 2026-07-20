3704ab3e4b19a0557ab6e31c5f9817ff400d6ac7
gustavomazzuco <gustavo_mazzuco@hotmail.com>
2026-07-20 18:27:57 -0300

    Aula 02

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,20 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from fastapi_zero.schemas import Message, UserSchema, UserPublic
+
+app = FastAPI(title = 'Minha API')
 
 
-@app.get('/')
+@app.get(
+    '/', 
+    status_code = HTTPStatus.OK,
+    response_model = Message
+)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+@app.post('/users/', status_code = HTTPStatus.CREATED, response_model = UserPublic)
+def create_user(user: UserSchema):
+    return user
