7f281d4612b461d5db625353b630704a92a95495
gustavomazzuco <gustavo_mazzuco@hotmail.com>
2026-07-23 18:24:12 -0300

    Aula 04

---
+++ tests/test_db.py
@@ -0,0 +1,16 @@
+from sqlalchemy import select
+
+from fastapi_zero.models import User
+
+
+def test_create_user(session):
+    new_user = User(
+        username='test', email='test@example.com', password='secret'
+    )
+
+    session.add(new_user)
+    session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'test'))
+
+    assert user.username == 'test'
